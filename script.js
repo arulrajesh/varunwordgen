6d3e75f726d54994db2068e91a5e232f5dbb14be
aruldesktop <aruldesktop>
2023-04-19 01:28:58 +0530

    first commit

--- script.js
+++ script.js
@@ -4,12 +4,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const voiceSelect = document.getElementById('voiceSelect');
     const startButton = document.getElementById('start');
     const stopButton = document.getElementById('stop');
+    const darkModeToggle = document.getElementById('darkModeToggle');
+    const countdownElement = document.getElementById('countdown');
+    const lastWordElement = document.getElementById('lastWord');
 
     let speechSynthesisInstance;
     let voices = [];
+    let countdownTimer;
 
     function populateVoiceList() {
         voices = speechSynthesis.getVoices();
+
+        if (voices.length === 0) {
+            setTimeout(populateVoiceList, 50);
+            return;
+        }
 
         voices.forEach(voice => {
             const option = document.createElement('option');
@@ -18,10 +27,28 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    if (typeof speechSynthesis !== 'undefined') {
-        if (speechSynthesis.onvoiceschanged !== undefined) {          speechSynthesis.onvoiceschanged = populateVoiceList;
-            populateVoiceList();
-        }
+    populateVoiceList();
+
+    function updateCountdown(secondsRemaining) {
+        countdownElement.textContent = secondsRemaining > 0 ? `(${secondsRemaining}s)` : '';
+    }
+
+    function startCountdown(seconds) {
+        let remainingTime = seconds;
+        updateCountdown(remainingTime);
+
+        countdownTimer = setInterval(() => {
+            remainingTime--;
+            updateCountdown(remainingTime);
+
+            if (remainingTime <= 0) {
+                clearInterval(countdownTimer);
+            }
+        }, 1000);
+    }
+
+    function updateLastWord(word) {
+        lastWordElement.textContent = word ? `| ${word} ` : '';
     }
 
     startButton.addEventListener('click', () => {
@@ -30,23 +57,32 @@ document.addEventListener('DOMContentLoaded', () => {
         const selectedVoice = voices.find(voice => voice.name === voiceSelect.value);
 
         clearInterval(speechSynthesisInstance);
+        clearInterval(countdownTimer);
 
         const speakWord = () => {
             const randomIndex = Math.floor(Math.random() * words.length);
-            const utterance = new SpeechSynthesisUtterance(words[randomIndex]);
+            const word = words[randomIndex];
+            const utterance = new SpeechSynthesisUtterance(word);
             utterance.voice = selectedVoice;
             speechSynthesis.speak(utterance);
+            updateLastWord(word);
+            startCountdown(selectedInterval / 1000);
         };
 
         speakWord();
         speechSynthesisInstance = setInterval(speakWord, selectedInterval);
         stopButton.disabled = false;
+        startButton.classList.add('btn-start-running');
     });
 
     stopButton.addEventListener('click', () => {
         clearInterval(speechSynthesisInstance);
+        clearInterval(countdownTimer);
         stopButton.disabled = true;
+        updateCountdown(0);
+        startButton.classList.remove('btn-start-running');
     });
+
     darkModeToggle.addEventListener('change', () => {
         document.body.classList.toggle('dark-mode');
     });
